fe0f639a715d025504d44253da471f06618964d7
Eduardo Cuducos <cuducos@gmail.com>
2016-08-31 18:50:40 -0300

    Setup static files directory

--- django_ajax/settings.py
+++ django_ajax/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+# Django REST Framework
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
